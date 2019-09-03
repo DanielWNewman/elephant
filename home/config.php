9e8bc1e4aae9ca7eaffd592d2410de3477c4d2d5
DanielWNewman <43504977+DanielWNewman@users.noreply.github.com>
2019-09-02 23:45:17 -0500

    Update config.php

--- home/config.php
+++ home/config.php
@@ -1,11 +1,6 @@
 # admin/config.php 
     <?php  
     // DB Settings 
-    define('DB_SERVER', 'localhost'); 
-    define('DB_USER', 'root'); 
-    define('DB_PASSWORD', ''); 
-    define('DB_NAME', 'nettuts_ns_demo'); 
-  
     define('FROM_EMAIL', 'no_reply@ohyeahemail.com'); 
     define('FROM_NAME', 'oh yeah email!'); 
   
