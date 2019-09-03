f19ef9c7fc6ac7e5094e1196354c88bfa74ab39d
DanielWNewman <43504977+DanielWNewman@users.noreply.github.com>
2019-09-02 23:47:19 -0500

    Create index.php

---
+++ home/index.php
@@ -0,0 +1,9 @@
+# admin/index.php 
+        <?php 
+        require_once 'config.php'; 
+        $title = "Home!"; 
+        $content = <<<EOF 
+        <h3>current stats</h3> 
+        Our home page! 
+        EOF; 
+        include 'layout.php'; ?>
